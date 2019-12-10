0c913d1d4112adcabaa92c9cf681ff0bf77f48f0
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-10 15:14:38 +0100

    add templates folder with heared, footer & menu

---
+++ application/views/templates/footer.php
@@ -0,0 +1,26 @@
+</div>
+</div>
+<!--/.container-->
+</div>
+<!--/.wrapper-->
+<div class="footer">
+    <div class="container">
+        <b class="copyright">&copy; 2019</b> All rights reserved.
+    </div>
+</div>
+<script type="text/javascript">
+var ajax_url = '<?= site_url() ?>';
+</script>
+
+<script src="<?= site_url('/assets/js/script.js') ?>" type="text/javascript"></script>
+<script src="<?= site_url('/assets/scripts/jquery-1.9.1.min.js') ?>" type="text/javascript"></script>
+<script src="<?= site_url('/assets/scripts/jquery-ui-1.10.1.custom.min.js') ?>" type="text/javascript"></script>
+<script src="<?= site_url('/assets/bootstrap/js/bootstrap.min.js') ?>" type="text/javascript"></script>
+<script src="<?= site_url('/assets/scripts/flot/jquery.flot.js') ?>" type="text/javascript"></script>
+<script src="<?= site_url('/assets/scripts/flot/jquery.flot.resize.js') ?>" type="text/javascript"></script>
+<script src="<?= site_url('/assets/scripts/datatables/jquery.dataTables.js') ?>" type="text/javascript"></script>
+<script src="<?= site_url('/assets/scripts/common.js') ?>" type="text/javascript"></script>
+
+</body>
+
+</html>
