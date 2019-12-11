3d38ec1a8a80e36d69742f2a96fc659d5f182b34
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-11 10:20:55 +0100

    add pdf-controller wit method create & display contract pdf

--- application/controllers/Pdf.php
+++ application/controllers/Pdf.php
@@ -5,8 +5,26 @@ class Pdf extends MY_Controller
     public function __construct()
     {
         parent::__construct();
-   
-       
+        $this->load->model('Contract_M');
+        $this->load->model('Employee_M');
+        $this->load->model('Bill_M');
+    }
+    function create_contract_pdf($id)
+    {
+        $data['contract'] = $this->Contract_M->get_by_id($id);
+        $uid = $data['contract']->uid;
+        $data['employee'] = $this->Employee_M->get_by_id($uid);
+        $this->load->view('templates/header');
+        $this->load->view('contract/pdf_contract', $data);
+    }
+
+    function display_contract_pdf($id)
+    {
+        $pdf = $this->Contract_M->get_by_id($id);
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename="file.pdf"');
+        echo $pdf->pdf;
+        exit();
     }
 
 
