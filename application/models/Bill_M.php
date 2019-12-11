9181aa275f13c5f4c873f9d9c86fb17551ac5e2b
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-11 11:10:43 +0100

    add method to Bill_M->valid_data

--- application/models/Bill_M.php
+++ application/models/Bill_M.php
@@ -10,13 +10,34 @@ class Bill_M extends CI_Model
         $this->load->model('Contract_M');
     }
 
-
-
     function get_by_id($id)
     {
         $this->db->from('bill')->where('id', $id);
         $res = $this->db->get()->result();
         return isset($res[0])  ? $res[0] : null;
     }
+
+    function valid_data()
+    {
+        $this->form_validation->set_rules('bill[bill_date]', 'Data wystawienia', 'required|trim');
+        $this->form_validation->set_rules('bill[netto]', 'Kwota netto', 'required|trim');
+        $this->form_validation->set_rules('bill[uid]', '', 'required|numeric');
+        $this->form_validation->set_rules('bill[cost_pcent]', '', 'required|numeric');
+        $this->form_validation->set_rules('bill[bank_transfer]', '', 'required|numeric');
+        if ($this->form_validation->run() !== false) {
+            $data['form_data'] = $this->input->post('bill');
+            $form_data = $data['form_data'];
+            $arr = [
+                'bill_date' => $form_data['bill_date'],
+                'netto' => $form_data['netto'],
+                'cost_pcent' => $form_data['cost_pcent'],
+                'bank_transfer' => $form_data['bank_transfer'],
+            ];
+            return $arr;
+        }
+        return null;
+    }
+
+
 
 }
