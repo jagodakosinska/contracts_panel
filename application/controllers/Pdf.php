72c59ef1756f10a11882e19cab117d75c91d0c68
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-11 10:23:53 +0100

    add method to Pdf Controller create&display bill

--- application/controllers/Pdf.php
+++ application/controllers/Pdf.php
@@ -26,6 +26,25 @@ class Pdf extends MY_Controller
         echo $pdf->pdf;
         exit();
     }
+    
+    function create_bill_pdf($id)
+    {
+        $data['bill'] = $this->Bill_M->get_by_id($id);
+        $data['contract'] = $this->Contract_M->show_by_bill_id($id);
+        $uid = $data['contract']->uid;
+        $data['employee'] = $this->Employee_M->get_by_id($uid);
+        $this->load->view('templates/header');
+        $this->load->view('bill/pdf_bill', $data);
+    }
+
+    function display_bill_pdf($id)
+    {
+        $pdf = $this->Bill_M->get_by_id($id);
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename="file.pdf"');
+        echo $pdf->pdf;
+        exit();
+    }
 
 
 }
