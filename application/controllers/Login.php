832adca9b285cad922d22329bf510feb42490be8
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-11 12:33:36 +0100

    add to Login method sing_up

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -16,5 +16,27 @@ class Login extends CI_Controller
         $this->load->view('templates/footer');
     }
 
-  
+    function sing_up()
+    {
+        $data['action'] = 'sing_up';
+        $data['submit'] = 'register';
+        if ($this->input->post('register')) {
+            $arr = $this->Login_M->valid_data();
+            if (!is_null($arr)) {
+                $res = $this->Login_M->check_user($arr);
+                //var_dump($res);
+                if(is_null($res)){
+                    $this->Login_M->add_user($arr);
+                    $this->session->set_flashdata('info', "Dodano nowego użytkownika");
+                    redirect('home');
+                }
+                $this->session->set_flashdata('info', "Taki użytkownik już istnieje!");
+               
+            }
+            $this->session->set_flashdata('error', "Nie udało się dodać użytkownika!");
+        }
+        $this->load_views($data);
+    }
+
+    
 }
